0afb55089828f02306cd8888f20c6133241c15fb
Marc Lemay <marc.lemay@disko.fr>
2015-04-16 11:57:31 +0200

    standardization of the translation of key
    just to have the same character case the entire translation chain

--- src/BaseEnum.php
+++ src/BaseEnum.php
@@ -9,45 +9,47 @@ abstract class BaseEnum extends LibraryEnum
 {
     /**
      * @param  mixed string|null a sprintf compatible pattern for generating labels
+     * @param  Closure  callback optional parameter modify pattern
      *
      * @return ChoiceList a symfony choice list, ready for use as the choice_list
      *                    option of a symfony choice widget
      */
-    public static function getChoiceList($pattern = null)
+    public static function getChoiceList($pattern = null, \Closure $callback = null)
     {
         $constants = self::getConstants();
 
         return new ChoiceList(
             $values = array_values($constants),
-            self::getLabelArray($values, $pattern)
+            self::getLabelArray($values, $pattern, $callback)
         );
     }
 
     /**
      * @param  mixed string|null a sprintf compatible pattern for generating labels
+     * @param  Closure  callback optional parameter modify pattern
      *
      * @return ChoiceList an associative array, ready for use with the choices
      *                    option of a symfony choice widget
      */
-    public static function getChoices($pattern = null)
+    public static function getChoices($pattern = null, \Closure $callback = null)
     {
         $constants = self::getConstants();
 
         return array_combine(
             $values = array_values($constants),
-            self::getLabelArray($values, $pattern)
+            self::getLabelArray($values, $pattern, $callback)
         );
     }
 
-    public static function getLabelArray($values, $pattern)
+    public static function getLabelArray($values, $pattern, \Closure $callback = null)
     {
         return $pattern === null ?
             $values:
             array_map(
-                function ($element) use ($pattern) {
+                function ($element) use ($pattern, $callback) {
                     return sprintf(
                         $pattern,
-                        $element
+                        ($callback)? $callback($element) : $element
                     );
                 },
                 $values
